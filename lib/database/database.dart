0476e88982749e5dcd8009831092a1299c87a200
SHIYAS253 <muhammedshiyas253@gmail.com>
2023-10-30 09:35:42 +0530

    first commit

---
+++ lib/database/database.dart
@@ -0,0 +1,11 @@
+// import 'package:crud/model/useremodels.dart';
+
+// class Database {
+//   static Map<String, dynamic> mymap = {
+//     "name": "hari",
+//     "address": "home",
+//     "institute": "luminar"
+//   };
+
+//   Newsss = Publicapiresponse.fromJson(mymap);
+// }
